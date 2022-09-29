25d9a56fa26335f965a24b310f3bf1f315ae064c
Eric <ericwtou@gmail.com>
2022-09-29 19:26:28 +0800

    Feed - Videos - Video Card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Box, selectClasses, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import { Sidebar, Videos } from '../components'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 
